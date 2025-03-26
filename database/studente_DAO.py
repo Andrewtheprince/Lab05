f86aa66dae92d497770f2d03b47219fbbb5fd40f
Andrewtheprince <andry.mazzo@outlook.com>
2025-03-26 19:34:08 +0100

    finito punto 1 e cominciato il 2

--- database/studente_DAO.py
+++ database/studente_DAO.py
@@ -1,3 +1,14 @@
-# Add whatever it is needed to interface with the DB Table studente
+from oauthlib.uri_validate import query
 
 from database.DB_connect import get_connection
+import mysql.connector
+
+class studenteDAO:
+
+    def __init__(self):
+        pass
+
+    def getStudentiCorso(self, corso):
+        cnx = get_connection()
+        cursor = cnx.cursor(dictionary=True)
+        query = """ SELECT * FROM """
